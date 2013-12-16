1e54ddede5d6758d59ed4b9630e40db768cc6a35
Andreas Hammar <andreas.hammar@jayway.com>
2013-12-16 23:43:01 +0100

    added projects

---
+++ src/JsonParsing.Core/DataParser.cs
@@ -0,0 +1,7 @@
+﻿namespace JsonParsing.Core
+{
+    public class DataParser
+    {
+         
+    }
+}
---
+++ src/JsonParsing.Tests/DataParserTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace JsonParsing.Tests
+{
+    [TestClass]
+    public class DataParserTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
